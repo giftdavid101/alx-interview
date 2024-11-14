d7ff3d0bb09a89256b3ea8a72b113a022904dccd
giftdavid101 <giftdavid101@gmail.com>
2024-11-14 14:17:45 +0100

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movie = process.argv[2];
+const api = 'https://swapi-api.hbtn.io/api/';
+const url = api + 'films/' + movie + '/';
+request.get({ url: url }, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    order(characters);
+  }
+});
+
+function order (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (err, res, body) {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        order(characters);
+      }
+    });
+  }
+}
